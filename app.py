65e752574d6b1c5de0324c477a107d19df73db89
Anas Bin Abbas <anas200002@diit.edu.bd>
2025-11-26 15:17:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -399,4 +399,5 @@ if __name__ == '__main__':
     if not os.path.exists(DATABASE):
         with app.app_context():
             init_db()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
